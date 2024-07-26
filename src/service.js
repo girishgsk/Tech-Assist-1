0803a01eedab0f70df9387930fc2464535cc9966
Girish Kumbhar <112197471+girishgsk@users.noreply.github.com>
2024-07-26 16:52:12 +0530

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosObj = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://13.49.226.200:8000",
   headers: {
     "Content-Type": "application/json",
   },
